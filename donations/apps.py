9f3f1a2d935006b8a7cab24724d2ff88e1d3dfc4
sohamgada95 <sohamgada2@gmail.com>
2025-07-30 21:01:31 +0530

    trial 2.

--- donations/apps.py
+++ donations/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class DonationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'donations'
+
+    def ready(self):
+        # Disable last_login updates to prevent database writes on Vercel
+        from django.contrib.auth.models import update_last_login
+        from django.contrib.auth import user_logged_in
+        user_logged_in.disconnect(update_last_login)
